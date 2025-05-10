0466337c83559607483da13e8604cd21dcb65ebd
Marcos-Dantas22 <marcosdantaslkdin@gmail.com>
2025-05-10 00:24:00 -0300

    build.sh modified

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Ativar o ambiente de trabalho do Poetry
+cd realmate_challenge && poetry install
+
+# Voltar uma pasta para executar os outros comandos
+cd ..
+
 # Realizar outras tarefas necessárias
 python manage.py collectstatic --no-input
 python manage.py migrate
@@ -20,27 +26,3 @@ if not User.objects.filter(username=username).exists():
 else:
     print(f'Superuser "{username}" já existe.')
 END
-
-# Ativar o ambiente de trabalho do Poetry
-cd realmate_challenge && poetry install
-
-# # Realizar outras tarefas necessárias
-# python manage.py collectstatic --no-input
-# python manage.py migrate
-
-# # Criar superusuário automaticamente
-# python manage.py shell << END
-# from django.contrib.auth import get_user_model
-
-# User = get_user_model()
-
-# username = 'useradmin'
-# password = 'adminadmin'
-# email = 'admin@example.com'
-
-# if not User.objects.filter(username=username).exists():
-#     User.objects.create_superuser(username=username, password=password, email=email)
-#     print(f'Superuser "{username}" criado com sucesso.')
-# else:
-#     print(f'Superuser "{username}" já existe.')
-# END
