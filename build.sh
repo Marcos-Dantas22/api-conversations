15771fe3ed12b64d0c578d5807c8f5c4e864ba42
Marcos-Dantas22 <marcosdantaslkdin@gmail.com>
2025-05-10 01:30:55 -0300

    modified build.sh

--- build.sh
+++ build.sh
@@ -11,18 +11,18 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Criar superusuário automaticamente
-python manage.py shell << END
-from django.contrib.auth import get_user_model
+# python manage.py shell << END
+# from django.contrib.auth import get_user_model
 
-User = get_user_model()
+# User = get_user_model()
 
-username = 'useradmin'
-password = 'adminadmin'
-email = 'admin@example.com'
+# username = 'useradmin'
+# password = 'adminadmin'
+# email = 'admin@example.com'
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, password=password, email=email)
-    print(f'Superuser "{username}" criado com sucesso.')
-else:
-    print(f'Superuser "{username}" já existe.')
-END
+# if not User.objects.filter(username=username).exists():
+#     User.objects.create_superuser(username=username, password=password, email=email)
+#     print(f'Superuser "{username}" criado com sucesso.')
+# else:
+#     print(f'Superuser "{username}" já existe.')
+# END
